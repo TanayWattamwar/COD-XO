6434d12798ce9be7afc3379bd65f23ddc8d1b6fe
TanayWattamwar <tanaywattamwar@gmail.com>
2024-09-08 16:11:04 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// JavaScript for form submission
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // Add more functionality like sending data to a server
+});
